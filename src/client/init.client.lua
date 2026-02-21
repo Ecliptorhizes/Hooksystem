624659c4bbc0e77403655420a4431f6ada7c6e26
Ecliptorhizes <gofers.chapel_2n@icloud.com>
2026-02-21 05:09:09 -0500

    Add Maid dependency and refactor project structure

--- src/client/init.client.lua
+++
@@ -1,5 +0,0 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Knit = require(ReplicatedStorage.Packages.Knit)
-
-Knit.Start():catch(warn):await()
--- React UI and controllers go here
--- src/shared/init.lua
+++
@@ -1,4 +0,0 @@
--- Shared modules and initialization
--- Require shared code from ReplicatedStorage here
-
-return nil
